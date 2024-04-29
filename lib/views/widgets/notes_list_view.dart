ce491839c2d5a6d93ff6b538a06e9937ad0d3bbf
mohamed <m.atia4285@gmail.com>
2024-04-29 15:16:40 +0300

    first commit

---
+++ lib/views/widgets/notes_list_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/constants.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
+import 'package:note_app/models/note_model.dart';
+
+import 'note_item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: notes.isEmpty ?const Center(
+            child:   Text('There is no note, write your note now',style: TextStyle(
+              color: kPrimaryColor,
+              fontWeight: FontWeight.w700,fontSize: 16
+            ),),
+          ) :
+          
+           ListView.builder(
+              itemCount: notes.length,
+              padding: EdgeInsets.zero,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: NoteItem(
+                    note: notes[index],
+                  ),
+                );
+              }),
+        );
+      },
+    );
+  }
+}
